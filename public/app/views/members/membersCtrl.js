90e5f51be0d05c355ffcecc6138dcb0e1d630358
Patrick Blair <pblair12@gmail.com>
2014-06-07 02:19:18 -0400

    members page is finished

--- public/app/views/members/membersCtrl.js
+++ public/app/views/members/membersCtrl.js
@@ -1,11 +1,19 @@
 angular.module('app').controller('membersCtrl', function($scope, $sce, memberService) {
+  $scope.selectedMember = undefined;
   $scope.selectedMemberHtml = "";
   memberService.getMembers().then(function(members) {
-    $scope.members = members.slice(0, (members.length / 2));
-    $scope.members2 = members.slice((members.length / 2), members.length);
+    $scope.allMembers = members;
+    $scope.membersColumn1 = members.slice(0, (members.length / 2));
+    $scope.membersColumn2 = members.slice((members.length / 2), members.length);
+    for (var i = 0; i < members.length; i++) {
+      if (members[i].name === "Adam Driscoll") {
+        $scope.selectMember(members[i]);
+      }
+    }
   })
 
   $scope.selectMember = function(member) {
+    $scope.selectedMember = member;
     memberService.getMember(member.name).then(function(memberHtml) {
       // use $sce.trustAsHtml to tell angular that the html received is 'safe' to display
       $scope.selectedMemberHtml = $sce.trustAsHtml(memberHtml);
