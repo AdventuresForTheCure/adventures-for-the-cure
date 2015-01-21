81037143bbd2eae10b2d62830de9bc58f285a8fc
Patrick Blair <pblair12@gmail.com>
2015-01-21 15:33:38 -0500

    fixing member select to work in IE

--- public/app/views/members/membersCtrl.js
+++ public/app/views/members/membersCtrl.js
@@ -38,7 +38,8 @@ function membersCtrl($scope, $location, $window, memberService, notifierService,
     $scope.selectedMember = member;
     var currHash = $window.location.hash.substring(1, $window.location.hash.length);
     if (currHash !== encodeURIComponent($scope.selectedMember.name)) {
-      $window.location.hash = $scope.selectedMember.name;
+//      $window.location.hash = $scope.selectedMember.name;
+      $location.hash($scope.selectedMember.name);
     }
   };
 
